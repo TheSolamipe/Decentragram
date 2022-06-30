0cc254d21ab15d11b9e6f0859b97ba28491b647d
Akinola Oloyede Solamipe <akinolasolamipe@gmail.com>
2022-06-30 20:59:38 +0100

    Smart contract completed

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -23,10 +23,27 @@ contract Decentragram {
         address payable author
     );
 
+    event ImageTipped(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
     // Create Posts
     function uploadImage(string memory _imgHash, string memory _description)
         public
     {
+        //make sure the image hash exists
+        require(bytes(_imgHash).length > 0);
+
+        //make sure the image description exists
+        require(bytes(_description).length > 0);
+
+        //make sure the Uploader address exists
+        require(msg.sender != address(0x0));
+
         //Increment image id
         imageCount++;
 
@@ -43,5 +60,33 @@ contract Decentragram {
         emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
     }
 
-    // Tip Posts
+    // Tip Images
+    function tipImageOwner(uint256 _id) public payable {
+        //Make sure the id is valid
+        require(_id > 0 && _id <= imageCount);
+
+        // fetch the Image
+        Image memory _image = images[_id];
+
+        // Fetch the author
+        address payable _author = _image.author;
+
+        // Pay the author by sending them ether
+        address(_author).transfer(msg.value);
+
+        // Increment the tip amount
+        _image.tipAmount = _image.tipAmount + msg.value;
+
+        // Update Image
+        images[_id] = _image;
+
+        // Trigger an event
+        emit ImageTipped(
+            _id,
+            _image.hash,
+            _image.description,
+            _image.tipAmount,
+            _author
+        );
+    }
 }
